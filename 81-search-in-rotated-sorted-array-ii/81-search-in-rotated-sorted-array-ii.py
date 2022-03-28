c18bf3e140553c3b48f3aee201ef40e377bb6af9
Hariharan <68813522+Hari0701@users.noreply.github.com>
2022-03-28 06:32:45 +0530

    Time: 98 ms (19.42%), Space: 14.6 MB (25.03%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
@@ -1,3 +1,4 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
-        return True if nums.count(target) > 0 else False
+        nums = list(set(nums))
+        return True if target in nums else False
