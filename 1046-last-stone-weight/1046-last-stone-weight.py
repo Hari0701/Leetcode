4b94cdd86b1dc97a08e356cd4c47e6f24ccf0006
Hariharan <68813522+Hari0701@users.noreply.github.com>
2022-04-07 18:50:58 +0530

    Time: 24 ms (99.21%), Space: 13.9 MB (17.07%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,10 +1,16 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
+#         sort the stones 
         stones = sorted(stones)
+    
         if(len(stones) == 1):
             return stones[0]
+        
         elif(len(stones) >= 2):
-            heaviestTwo = stones[-1] - stones[-2]
-            stones = stones[:len(stones)-2]
-            stones.append(heaviestTwo)
+            y = stones[-1]
+            stones.pop()
+            x = stones[-1]
+            stones.pop()
+            stones.append(y - x)
+            
         return self.lastStoneWeight(stones)    
