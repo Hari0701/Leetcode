2ec3b381412d9ef0c429fb8d9c8f560372f72cbf
Hariharan <68813522+Hari0701@users.noreply.github.com>
2022-07-17 23:26:58 +0530

    Time: 78 ms (95.11%), Space: 19.8 MB (95.74%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -7,7 +7,7 @@ public:
         
         for(int i = 0;i< nums.size() - 2; i++){
             
-            if(i==0|| nums[i]!=nums[i-1]){
+            if(i==0 || nums[i]!=nums[i-1]){
             start = i + 1;
             end = nums.size() - 1;
                 
@@ -15,12 +15,14 @@ public:
                 sum = nums[i] + nums[start] + nums[end];
                 if(sum == 0){
                     triplets.push_back({nums[i], nums[start], nums[end]});
+                    
                         while(start<end && nums[start]==nums[start+1]){
                             start++;
                         }
                         while(start<end && nums[end]==nums[end-1]){
                             end--;
                         }
+                    
                         start++;
                         end--;
                 }
